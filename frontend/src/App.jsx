514d4d99a0deb95ec42f46f5b0e7564b5901ac3d
Sergio Olivas <siohd4@umsystem.edu>
2026-03-09 23:36:44 -0500

    add login page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from "react"
 import Login from "./components/Login"
 import Register from "./components/Register"
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [showRegister, setShowRegister] = useState(false)
 
-  return (
-    <div>
-      {showRegister ? (
-        <Register onSwitch={() => setShowRegister(false)} />
-      ) : (
-        <Login onSwitch={() => setShowRegister(true)} />
-      )}
-    </div>
-  )
+  return showRegister
+    ? <Register onSwitch={() => setShowRegister(false)} />
+    : <Login onSwitch={() => setShowRegister(true)} onLogin={() => {}} />
 }
 
 export default App
